eea64844fabffb84d7cb26f821e2835d43781a61
Simon Kristopher Kruse Bentzen <Simonkruse2@gmail.com>
2019-11-20 12:41:36 +0100

    Added img and img.thumbnail css.

--- sem3Front/src/components/Team.js
+++ sem3Front/src/components/Team.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
-
+import "../App.css";
 function Team({ teamName }) {
   console.log("Team");
   return (
     <div>
       <p>Teeeaaaam</p>
       <p>Chosen Team = {teamName.teamName}</p>
-      <img src="https://upload.wikimedia.org/wikipedia/en/5/53/Arsenal_FC.svg"></img>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/en/5/53/Arsenal_FC.svg"
+        className="thumbnail"
+      ></img>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/en/5/53/Arsenal_FC.svg"
+        className="img"
+      ></img>
     </div>
   );
 }
