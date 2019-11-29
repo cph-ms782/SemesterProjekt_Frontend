7fdeb3c8d75a0abe8f55e4feac26478af2d35d4c
Martin Sander-Thomsen <cph-ms782@cphbusiness.dk>
2019-11-29 16:36:00 +0100

    news menu shown in left corner and text underneath

--- sem3Front/src/components/News.js
+++ sem3Front/src/components/News.js
@@ -14,43 +14,47 @@ import NewsAirports from "./news/NewsAirports";
 import NewsNodes from "./news/NewsNodes";
 import NewsFlights from "./news/NewsFlights";
 
-function News({ airports, teams, flightHomeCity, flightAwayCity, teamMatches,teamDates, flightTime, flightDate, updateTicketURL, updateShowBuyImage, dummyAir, updateTeamPlayers, teamID }) {
+function News({ airports, teams, flightHomeCity, flightAwayCity, teamMatches, teamDates, flightTime, flightDate, updateTicketURL, updateShowBuyImage, dummyAir, updateTeamPlayers, teamID }) {
   console.log("News");
   return (
     <div>
       <Router >
-    <div>
-        <ul className="header">
-          <li>
-          <NavLink exact activeClassName="active" to="/all">
-            Players
+        <div className="newsContainer">
+          <div>
+            <ul className="newsHeader" >
+              <li>
+                <NavLink exact activeClassName="active" to="/all">
+                  Players
           </NavLink>
-          </li>
-        </ul>
-    </div>
-        <Switch>
-          <Route exact path="/api"><NewsApi /></Route>
-          <Route exact path="/airports"><NewsAirports airports={airports} /></Route>
-          <Route exact path="/teams"><NewsTeams teams={teams} /></Route>
-          <Route exact path="/nodes"><NewsNodes /></Route>
-          <Route exact path="/flights"><NewsFlights
-          teamDates={teamDates} 
-          teamMatches={teamMatches} 
-          flightAwayCity={flightAwayCity} 
-            teams={teams}
-            airports={airports}
-            flightHomeCity={flightHomeCity}
-            flightTime={flightTime}
-            flightDate={flightDate}
-            updateTicketURL={updateTicketURL}
-            updateShowBuyImage={updateShowBuyImage}
-            dummyAir={dummyAir}
-          /></Route>
-          <Route exact path="/all"><AllAboutTeam updateTeamPlayers={updateTeamPlayers} teamID={teamID}/></Route>
-          <Route exact path="/"><NewsFileForTesting teams={teams} /></Route>
-          <Route ><NoMatch /></Route>
-        </Switch>
-      
+              </li>
+            </ul>
+          </div>
+          <br /><br /><br />
+          <div className="newsContent">
+            <Switch>
+              <Route exact path="/api"><NewsApi /></Route>
+              <Route exact path="/airports"><NewsAirports airports={airports} /></Route>
+              <Route exact path="/teams"><NewsTeams teams={teams} /></Route>
+              <Route exact path="/nodes"><NewsNodes /></Route>
+              <Route exact path="/flights"><NewsFlights
+                teamDates={teamDates}
+                teamMatches={teamMatches}
+                flightAwayCity={flightAwayCity}
+                teams={teams}
+                airports={airports}
+                flightHomeCity={flightHomeCity}
+                flightTime={flightTime}
+                flightDate={flightDate}
+                updateTicketURL={updateTicketURL}
+                updateShowBuyImage={updateShowBuyImage}
+                dummyAir={dummyAir}
+              /></Route>
+              <Route exact path="/all"><AllAboutTeam updateTeamPlayers={updateTeamPlayers} teamID={teamID} /></Route>
+              <Route exact path="/"><NewsFileForTesting teams={teams} /></Route>
+              <Route ><NoMatch /></Route>
+            </Switch>
+          </div>
+        </div>
       </Router>
     </div>
   );
